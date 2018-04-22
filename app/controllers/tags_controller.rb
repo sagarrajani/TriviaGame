13dc325f94c7ef71ce337e0f2ac6e72d59d108fa
Sagar Rajani <sagar121311@gmail.com>
2018-04-22 19:24:52 -0400

    multiple controllers (#4)

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,60 @@
 class TagsController < ApplicationController
+  
   def index
     @tags=ActsAsTaggableOn::Tag.all
   end
+
+  def question
+    @question = Question.tagged_with(params[:tag])
+    @question = @question.paginate(:page => params[:page], :per_page => 5)
+    @tag=params[:tag]
+    # @tagw="tag"
+  end
+  
+  def show
+    @question=Question.find(params[:id])
+    @answer=Question.new
+  end
+
+  def answer
+    @tag=params[:name]
+    @question=Question.find(params[:id])
+    @answer=Question.new(response_params)
+    check = compare(@question.answer.to_s.downcase, @answer.answer.to_s.downcase)
+    score = 0
+    if check
+      score = 4
+      @question.attempts.create(user_id: current_user.id,question_id: @question.id)
+    else
+      score = -1
+    end
+    user = User.find(current_user.id)
+    if (user.score == nil && score == -1)
+      user.update_attribute(:score, 0)
+    elsif user.score == nil && score == 4
+      user.update_attribute(:score, score)
+    else
+      user.update_attribute(:score, user.score + score)
+      if user.score < 0
+        user.update_attribute(:score, 0)
+      end
+    end
+  end
+
+  private
+      def post_params
+        params.require(:question).permit(:question, :answer, :tag_list)
+      end
+
+      def compare(question, answer)
+        return answer == question
+      end
+
+      def response_params
+        params.require(:question).permit(:question,:answer,:user_id)
+      end
+      
+      def response_params2
+        params.permit(:question,:answer)
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,20 +9,20 @@ Rails.application.routes.draw do
   # get 'categories' => 'categories#index', as: 'category'
   # get 'categories' => 'categories#index', as: 'categories'
   # get '/categories/:category_id', to:  'questions#new#'
-  get 'question' => 'questions#my_index' , as: 'my_question'
-  get 'question/:id/' => 'questions#my_show' , as: 'show_my_question'
-  delete 'question.:id/',to: 'questions#destroy', as: 'delete_my_question'
-  get 'questions/:id/edit', to: 'questions#edit', as: 'edit_my_questions'
+  # get 'question' => 'myquestions#my_index' , as: 'my_question'
+  # get 'question/:id/' => 'myquestions#my_show' , as: 'show_my_question'
+  # delete 'question.:id/',to: 'myquestions#destroy', as: 'delete_my_question'
+  # get 'questions/:id/edit', to: 'myquestions#edit', as: 'edit_my_questions'
   # get 'questions/:id/answer', to: 'questions#answer', as: 'ganswer'
   post 'questions/:id/answer', to: 'questions#answer', as: 'answer'
-  patch 'questions/:id/edit', to: 'questions#update'
-  get 'tags/:tag', to: 'questions#tag_index', as: 'tag'
-  get 'tags/:name/tagquestions/:id', to: 'questions#tag_show', as: 'tag_show'
-  post 'tags/:name/tagquestions/:id/answer', to: 'questions#tag_answer', as: 'tag_answer'
+  patch 'myquestions/:id/edit', to: 'myquestions#update'
+  get 'tags/:tag', to: 'tags#question', as: 'tag'
+  get 'tags/:name/tagquestions/:id', to: 'tags#show', as: 'tag_show'
+  post 'tags/:name/tagquestions/:id/answer', to: 'tags#answer', as: 'tag_answer'
   get 'users/score', to: 'users#score', as: 'score'
 
-  get 'round', to: 'questions#round_index', as: 'round_index'
-  get 'round/:id', to: 'questions#round', as: 'round'
+  # get 'round', to: 'questions#round_index', as: 'round_index'
+  # get 'round/:id', to: 'questions#round', as: 'round'
 
   # match '/users',   to: 'users#index',   via: 'get'
   # match '/user/:id', to: 'users#show',       via: 'get'
@@ -34,6 +34,9 @@ Rails.application.routes.draw do
     member { post :vote }
   end
   resources :tags
+  resources :myquestions
+  resources :rounds
+  # post 'myquestions', to: 'myquestions#create'
   # resources :users, except: [:new,:create]
     # resources :categories, except: [:destroy]
 # resources :categories
